698090e6b1005647b5d7a6e5aabb9cc45af9c3f7
jorge <jorgemalaquia@gmail.com>
2023-01-18 21:03:47 -0300

    build: models added

---
+++ src/main/java/com/tweteroo/api/models/Tweter.java
@@ -0,0 +1,27 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Tweter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String userName;
+    
+    @Column(nullable = false)
+    private String avatar;
+
+    @Column(length = 150, nullable = false)
+    private String text;
+}
