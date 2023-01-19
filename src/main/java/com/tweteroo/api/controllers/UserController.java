d0b9b7d2e5b6041489cc8b08d1e5e274de29b7c1
jorge <jorgemalaquia@gmail.com>
2023-01-18 21:31:41 -0300

    feat: route get users to test the post route

--- src/main/java/com/tweteroo/api/controllers/UserController.java
+++ src/main/java/com/tweteroo/api/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.tweteroo.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +14,18 @@ import com.tweteroo.api.models.TweterooUser;
 import com.tweteroo.api.repositories.UserRepository;
 
 @RestController
-@RequestMapping("/sign-up")
+@RequestMapping("/")
 public class UserController {
     
     @Autowired
     private UserRepository repository;
 
-    @PostMapping
+    @GetMapping("/users")
+    public List<TweterooUser> listAll(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/sign-up")
     public void signUp(@RequestBody UserDTO reqBody){
         repository.save(new TweterooUser(reqBody));
     }
